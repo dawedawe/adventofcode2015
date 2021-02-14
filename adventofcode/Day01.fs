57d88a2841e6295a1e8e963be4eb34498665bc2e
dawe <dawedawe@gmx.de>
2021-02-14 23:44:29 +0100

    Day 1 Part 2

--- adventofcode/Day01.fs
+++ adventofcode/Day01.fs
@@ -9,3 +9,23 @@ let day01 () =
     |> fun s -> s.ToCharArray()
     |> Array.countBy (fun c -> if c = '(' then 1 else (-1))
     |> Array.sumBy (fun x -> fst x * snd x)
+
+let rec move currentFloor c position =
+    if (Array.isEmpty c) then
+        failwith "empty input"
+    let currentFloor' =
+        if c.[0] = '('
+        then currentFloor + 1
+        else currentFloor - 1
+    let position' = position + 1
+    if currentFloor' = -1
+    then position'
+    else move currentFloor' c.[1..] position'
+
+let day01Part2 () =
+    let input =
+        InputFile
+        |> System.IO.File.ReadAllText
+        |> fun s -> s.ToCharArray()
+
+    move 0 input 0
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -5,6 +5,6 @@ open Day01
 
 [<EntryPoint>]
 let main argv =
-    let r = Day01.day01()
+    let r = Day01.day01Part2()
     printfn "%A" r 
     0 // return an integer exit code
