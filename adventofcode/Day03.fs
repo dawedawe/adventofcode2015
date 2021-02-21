1812d9825528dc51400b0c6c1c84cf02767ca34d
dawe <dawedawe@gmx.de>
2021-02-21 23:45:36 +0100

    Day 3 Part 2

--- adventofcode/Day03.fs
+++ adventofcode/Day03.fs
@@ -23,8 +23,22 @@ let move (line: string) =
     let positions = Set.singleton startingPos
     helper positions startingPos (line.ToCharArray())
 
-let day03() =
+let day03 () =
     InputFile
     |> System.IO.File.ReadAllText
     |> move
     |> Set.count
+
+let day03Part2 () =
+    let (part0, part1) =
+        InputFile
+        |> System.IO.File.ReadAllText
+        |> fun s -> s.ToCharArray()
+        |> Array.indexed
+        |> Array.partition (fun (i, _) -> i % 2 = 0)
+
+    let input0 = part0 |> Array.map snd |> System.String
+    let input1 = part1 |> Array.map snd |> System.String
+    let pos0 = move input0
+    let pos1 = move input1
+    Set.union pos0 pos1 |> Set.count
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -4,6 +4,6 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    let r = Day03.day03()
+    let r = Day03.day03Part2()
     printfn "%A" r 
     0 // return an integer exit code
