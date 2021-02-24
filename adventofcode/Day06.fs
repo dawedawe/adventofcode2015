c47c90b48865c28d4717c75ac6dc81ea61a3cc7c
dawe <dawedawe@gmx.de>
2021-02-24 22:11:08 +0100

    Day 6 Part 2

--- adventofcode/Day06.fs
+++ adventofcode/Day06.fs
@@ -59,3 +59,29 @@ let day06 () =
                 if grid.[x, y] then yield 1
     }
     |> Seq.length
+
+let gridTransformPart2 (grid: int [,]) ((c1x, c1y), (c2x, c2y)) f =
+    for x = c1x to c2x do
+        for y = c1y to c2y do
+            grid.[x, y] <- f grid.[x, y]
+
+    grid
+
+let doInstructionPart2 (grid: int [,]) instr =
+    match instr with
+    | TurnOn (c1, c2) -> gridTransformPart2 grid (c1, c2) (fun x -> x + 1)
+    | TurnOff (c1, c2) -> gridTransformPart2 grid (c1, c2) (fun x -> max (x - 1) 0)
+    | Toggle (c1, c2) -> gridTransformPart2 grid (c1, c2) (fun x -> x + 2)
+
+let day06Part2 () =
+    let grid =
+        InputFile
+        |> System.IO.File.ReadAllLines
+        |> Array.map parse
+        |> Array.fold doInstructionPart2 (Array2D.create 1000 1000 0)
+
+    seq {
+        for x = 0 to Array2D.length1 grid - 1 do
+            yield Array.sum grid.[x, *]
+    }
+    |> Seq.sum
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -4,6 +4,6 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    let r = Day06.day06()
+    let r = Day06.day06Part2()
     printfn "%A" r 
     0 // return an integer exit code
