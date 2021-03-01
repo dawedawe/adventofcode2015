28fd511eaa7e68b15981c431dee498eda37e25cc
dawe <dawedawe@gmx.de>
2021-03-01 07:39:40 +0100

    Day 10 Part 2

--- adventofcode/Day10.fs
+++ adventofcode/Day10.fs
@@ -1,30 +1,24 @@
 module Day10
 
 let getRun (s: string) =
-    s.ToCharArray()
-    |> Array.takeWhile (fun c -> c = s.[0])
-    |> fun a -> System.String.Join(null, a)
-
-let transform (s: string) =
-    if (s.Length > 9) then
-        failwith "unsupported length"
-
-    let count = s.Length.ToString()
-    let digit = string s.[0]
-    count + digit
+    if s.Length >= 3 && s.[0] = s.[1] && s.[1] = s.[2]
+    then s.Substring(0, 3)
+    else if s.Length >= 2 && s.[0] = s.[1]
+    then s.Substring(0, 2)
+    else string s.[0]
 
 let expand (s: string) =
-    let rec helper (sofar: string) index =
+    let b = System.Text.StringBuilder()
+    let rec helper index =
         if index = s.Length then
-            sofar
+            b.ToString()
         else
             let toExpand = getRun s.[index..]
-            let expanded = transform toExpand
-            let sofar' = sofar + expanded
-            let index' = (index + toExpand.Length)
-            helper sofar' index'
+            b.Append(toExpand.Length.ToString()) |> ignore
+            b.Append(string toExpand.[0]) |> ignore
+            helper (index + toExpand.Length)
 
-    helper "" 0
+    helper 0
 
 let rec lookAndSay n input =
     if n = 0 then
@@ -36,4 +30,9 @@ let rec lookAndSay n input =
 let day10 () =
     let input = "1321131112"
     let output = lookAndSay 40 input
+    output.Length
+
+let day10Part2 () =
+    let input = "1321131112"
+    let output = lookAndSay 50 input
     output.Length
