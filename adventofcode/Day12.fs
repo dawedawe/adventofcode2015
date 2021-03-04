0327789c2fd7f6b17340763a00b796833a9173af
dawe <dawedawe@gmx.de>
2021-03-04 22:17:26 +0100

    Day 12 Part 2

--- adventofcode/Day12.fs
+++ adventofcode/Day12.fs
@@ -1,5 +1,7 @@
 module Day12
 
+open System.Text.Json
+
 [<Literal>]
 let InputFile = "Day12Input.txt"
 
@@ -23,3 +25,52 @@ let sum (s: string) =
 
 let day12 () =
     InputFile |> System.IO.File.ReadAllText |> sum
+
+let hasRedProperty (jo: JsonElement) =
+    jo.EnumerateObject()
+    |> Seq.tryFind
+        (fun p ->
+            p.Value.ValueKind = JsonValueKind.String
+            && p.Value.GetString() = "red")
+    |> Option.isSome
+
+let rec sumObject (jo: JsonElement) =
+    if hasRedProperty jo then
+        0
+    else
+        seq {
+            for p in jo.EnumerateObject() do
+                match p.Value.ValueKind with
+                | JsonValueKind.Array -> yield sumArray p.Value
+                | JsonValueKind.Object -> yield sumObject p.Value
+                | JsonValueKind.String -> yield 0
+                | JsonValueKind.Number -> yield p.Value.GetInt32()
+                | _ -> failwith "unsupported ValueKind"
+        }
+        |> Seq.sum
+
+and sumArray (ja: JsonElement) =
+    seq {
+        for je in ja.EnumerateArray() do
+            match je.ValueKind with
+            | JsonValueKind.Array -> yield sumArray je
+            | JsonValueKind.Object -> yield sumObject je
+            | JsonValueKind.String -> yield 0
+            | JsonValueKind.Number -> yield je.GetInt32()
+            | _ -> failwith "unsupported ValueKind"
+    }
+    |> Seq.sum
+
+let sum2 (je: JsonElement) =
+    match je.ValueKind with
+    | JsonValueKind.Array -> sumArray je
+    | JsonValueKind.Object -> sumObject je
+    | JsonValueKind.String -> 0
+    | JsonValueKind.Number -> je.GetInt32()
+    | _ -> failwith "unsupported ValueKind"
+
+let day12Part2 () =
+    InputFile
+    |> System.IO.File.ReadAllText
+    |> System.Text.Json.JsonDocument.Parse
+    |> fun d -> sum2 d.RootElement
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -4,6 +4,6 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    let r = Day12.day12()
+    let r = Day12.day12Part2()
     printfn "%A" r 
     0 // return an integer exit code
