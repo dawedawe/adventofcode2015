10d6be63a9ec4c7dc2ff98f3de5481de13c86fa4
dawe <dawedawe@gmx.de>
2021-03-11 23:00:46 +0100

    Day 15 Part 1

---
+++ adventofcode/Day15.fs
@@ -0,0 +1,67 @@
+module Day15
+
+[<Literal>]
+let InputFile = "Day15Input.txt"
+
+type Ingredient =
+    { Name: string
+      Capacity: int
+      Durability: int
+      Flavor: int
+      Texture: int
+      Calories: int }
+
+let parse (s: string) =
+    let words = s.Split(' ')
+
+    { Name = words.[0].Trim(':')
+      Capacity = int (words.[2].Trim(','))
+      Durability = int (words.[4].Trim(','))
+      Flavor = int (words.[6].Trim(','))
+      Texture = int (words.[8].Trim(','))
+      Calories = int (words.[10].Trim(',')) }
+
+let calcScore (recipe: seq<Ingredient * int>) =
+    let capacity =
+        recipe
+        |> Seq.sumBy (fun (i, c) -> c * i.Capacity)
+        |> max 0
+
+    let durability =
+        recipe
+        |> Seq.sumBy (fun (i, c) -> c * i.Durability)
+        |> max 0
+
+    let flavor =
+        recipe
+        |> Seq.sumBy (fun (i, c) -> c * i.Flavor)
+        |> max 0
+
+    let texture =
+        recipe
+        |> Seq.sumBy (fun (i, c) -> c * i.Texture)
+        |> max 0
+
+    capacity * durability * flavor * texture
+
+let findHighScore (ingredients: Ingredient []) =
+    seq {
+        for i in 0 .. 100 do
+            for j in 0 .. (100 - i) do
+                for k in 0 .. (100 - i - j) do
+                    for l in 0 .. (100 - i - j - k) do
+                        let recipe =
+                            [ (ingredients.[0], i)
+                              (ingredients.[1], j)
+                              (ingredients.[2], k)
+                              (ingredients.[3], l) ]
+
+                        yield (calcScore recipe)
+    }
+    |> Seq.max
+
+let day15 () =
+    InputFile
+    |> System.IO.File.ReadAllLines
+    |> Array.map parse
+    |> findHighScore
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -4,6 +4,6 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    let r = Day14.day14Part2()
+    let r = Day15.day15()
     printfn "%A" r 
     0 // return an integer exit code
